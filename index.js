4ebb89eddfa0fba4eafc3605d81c88308af83394
Sean Altemose <sean@seanaltemose.com>
2019-04-24 19:37:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function currentLine(line){
   var lineArray = []
   var i = 0
   for (i=0; i < line.length; i++){
-    lineArray.push(`${[i+1]}. ${line[i]}`)
-    lineArray.join(", ")
-    lineSentence.push(lineArray)
+    lineArray.push(` ${[i+1]}. ${line[i]}`)
   };
+  lineArray.join(", ")
+  lineSentence.push(lineArray)
   return lineSentence.join(" ")
 }

09374d21960bff5012dcc346d3e27b7f6cbcd0a6
Sean Altemose <sean@seanaltemose.com>
2019-04-24 19:24:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function takeANumber(line, name){
 function nowServing(line){
   while (line.length > 0){
     return `Currently serving ${line[0]}.`
-    return line.shift()
+    line.shift()
+    line
   }
   return "There is nobody waiting to be served!"
 }

ee16a9161f0e49174248c6bf2566bd9f70eeaa72
Sean Altemose <sean@seanaltemose.com>
2019-04-24 19:44:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ function nowServing(line){
 }
 
 function currentLine(line){
+  if (line.length == 0)
+    return "The line is currently empty."
   var lineSentence = ["The line is currently:"]
   var lineArray = []
   var i = 0
   for (i=0; i < line.length; i++){
-    if (line.length > 0 ) {
     lineArray.push(` ${[i+1]}. ${line[i]}`)
+  }
       lineArray.join(", ")
       lineSentence.push(lineArray)
       return lineSentence.join("")
-    }
-      return "The line is currently empty."
-    }
   }
+}
